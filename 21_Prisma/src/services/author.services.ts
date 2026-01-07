65f304cd9c7e3dea3f5f7658d2bae9fcc286f0f9
soumadip-dev <soumadipmajila@gmail.com>
2026-01-08 00:15:42 +0530

    feat: add service methods to create and delete authors

--- 21_Prisma/src/services/author.services.ts
+++ 21_Prisma/src/services/author.services.ts
@@ -1,1 +1,34 @@
 import { prisma } from '../../prisma';
+
+//* Create a new author
+async function addAuthor(name: string) {
+  try {
+    const createdAuthor = await prisma.author.create({
+      data: {
+        name,
+      },
+    });
+
+    return createdAuthor;
+  } catch (error) {
+    console.error('Error creating author ❌', error);
+    throw error;
+  }
+}
+
+//* Delete an author by ID along with related books
+async function deleteAuthor(id: number) {
+  try {
+    const deletedAuthor = await prisma.author.delete({
+      where: { id },
+      include: { books: true },
+    });
+
+    return deletedAuthor;
+  } catch (error) {
+    console.error('Error deleting author ❌', error);
+    throw error;
+  }
+}
+
+export { addAuthor, deleteAuthor };
