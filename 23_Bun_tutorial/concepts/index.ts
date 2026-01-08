fa994271e2a3460d1a8cce1f308b7ac401cc2560
soumadip-dev <soumadipmajila@gmail.com>
2026-01-08 15:12:28 +0530

    feat: learn and implement server creation using Bun.serve method

--- 23_Bun_tutorial/concepts/index.ts
+++ 23_Bun_tutorial/concepts/index.ts
@@ -5,18 +5,6 @@ import express, { type Request, type Response } from 'express';
 // import './features/fs';
 // import './features/binary';
 // import './features/meta-env';
-import './features/fetch';
-
-const port: number = 8080;
-const app = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({
-    message: 'First express app with bun',
-    success: true,
-  });
-});
-
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+// import './features/fetch';
+// import './features/fetch';
+import './features/server';
