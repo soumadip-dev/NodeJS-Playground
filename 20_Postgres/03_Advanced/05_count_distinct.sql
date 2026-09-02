08d2df6fb4a2ce5c293c889ae1924b2de94d8d3e
Soumadip Majila <soumadipmajila@gmail.com>
2026-09-02 21:35:26 +0530

    feat: learn about count distinct

---
+++ 20_Postgres/03_Advanced/05_count_distinct.sql
@@ -0,0 +1,16 @@
+-- Count unique values
+
+-- Count how many unique posts are connected to each tag
+SELECT 
+    t.name AS tag_name,
+    COUNT(DISTINCT p.id) AS total_unique_posts
+FROM advanced.tags AS t
+LEFT JOIN advanced.posts_tags AS pt
+    ON t.id = pt.tag_id
+LEFT JOIN advanced.posts AS p
+    ON pt.post_id = p.id
+GROUP BY 
+    t.id, 
+    t.name
+ORDER BY 
+    total_unique_posts DESC;
